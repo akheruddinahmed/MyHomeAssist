ed85252d5899dd5e6395a73391cc156264b39ca6
AKHER UDDIN AHMED <117501410+akheruddinahmed@users.noreply.github.com>
2026-07-13 12:06:46 +0530

    Remove Gallery import from Home.tsx
    Removed Gallery import from Home component.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { WhyChooseUs } from "@/components/sections/WhyChooseUs";
 import { PricingSection } from "@/components/sections/PricingSection";
 import { HowItWorks } from "@/components/sections/HowItWorks";
 import { CleaningChecklist } from "@/components/sections/CleaningChecklist";
-import { Gallery } from "@/components/sections/Gallery";
+
 import { Faq } from "@/components/sections/Faq";
 import { ServiceAreas } from "@/components/sections/ServiceAreas";
 import { BookNowCta } from "@/components/sections/BookNowCta";
