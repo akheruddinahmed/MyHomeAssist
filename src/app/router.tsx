8f31e76b4ebe5b27b833803f35d78bad0cb74257
AKHER UDDIN AHMED <117501410+akheruddinahmed@users.noreply.github.com>
2026-07-15 09:34:51 +0530

    Add bathroom cleaning route to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -21,6 +21,10 @@ export const router = createBrowserRouter([
         lazy: () => import("@/pages/HomeDeepCleaning"),
       },
       {
+        path: "bathroom-cleaning",
+        lazy: () => import("@/pages/BathroomCleaning"),
+      },
+      {
         path: "pricing",
         lazy: () => import("@/pages/Pricing"),
       },
